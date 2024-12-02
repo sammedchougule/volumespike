b4b52219b62767bd768b283342381f5d8f677e07
sammedchougule <smdchougule24@gmail.com>
2024-12-02 14:30:53 +0530

    "solved the error of type 'any' "

--- types/stock.tsx
+++ types/stock.tsx
@@ -8,6 +8,26 @@ export interface Stock {
     sector: string
     subsector?: string
     logo?: string
+    view_chart: string;
+    website_link: string;
+    stock_name: string;
+    industry: string;
+    exchange: string;
+    close_yest: number;
+    price_open: number;
+    low: number;
+    high: number;
+    volume: number;
+    avg_volume: number;
+    volume_spike: number;
+    month_high: number;
+    month_low: number;
+    month_hl_cross: string;
+    year_high: number;
+    year_low: number;
+    year_hl_cross: string;
+    marketcap: number; // Market cap might need formatting (e.g., with "B" or "M")
+    eps: number; // Earnings per share
   }
   
   export interface SectorGroup {

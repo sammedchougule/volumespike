0ca9bf1208e3848e22a373d5efc5e603605acc28
Sammed Chougule <51811899+sammedchougule@users.noreply.github.com>
2024-12-04 20:53:50 +0530

    Update0 page.tsx

--- app/intrabuzz/page.tsx
+++ app/intrabuzz/page.tsx
@@ -122,7 +122,7 @@ export default function Nifty50Page() {
       </div>
 
 
-      <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
         {filteredStocks.map((stock) => (
           <div
             key={stock.symbol}
